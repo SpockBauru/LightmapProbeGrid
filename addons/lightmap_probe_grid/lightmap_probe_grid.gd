77350282220f92f4b092d7fd82f123d78b9aa1e1
SpockBauru <67930504+SpockBauru@users.noreply.github.com>
2024-03-30 18:27:19 -0300

    v2.0: GPU Raycast - No colliders needed
    Major changes:
    Using GPU Raycast workaround: Cameras with depth filter can be used to detect objects instead of colliders.
    - No colliders: Only visual objects are needed
    - Now visual culling mask is used: Not compatible with v1.0 masks!
    - Slower but accurate enough

--- addons/lightmap_probe_grid/lightmap_probe_grid.gd
+++ addons/lightmap_probe_grid/lightmap_probe_grid.gd
@@ -7,7 +7,9 @@ signal probes_changed
 
 const max_probes: int = 1000
 
-@export_flags_3d_physics var collision_mask: int = 1
+## Only selected layers will be seen by LightmapProbeGrid. Works like Camera3D Cull Mask.[br][br]
+## NOTE: NOT compatible with LightmapProbeGrid v1.0
+@export_flags_3d_render var visual_cull_mask: int = 1048575
 
 @export var size: Vector3 = Vector3.ONE:
 	set(value):
@@ -16,6 +18,8 @@ const max_probes: int = 1000
 		size_changed.emit()
 	get: 
 		return size
+
+@onready var depth_shader: Shader = preload("Depth.gdshader")
 
 var probes_number: Vector3i = Vector3i(2, 2, 2):
 	set(value):
@@ -149,11 +153,11 @@ func generate_probes() -> void:
 	var step_y: float = size.y / (probes_y - 1)
 	var step_z: float = size.z / (probes_z - 1)
 	
-	# Starting positions
+	# Starting relative positions
 	var start_position: Vector3 = Vector3.ONE * size / 2.0
 	var current_position: Vector3 = Vector3.ZERO
 	
-	# Defining Probes positions and names
+	# Defining Probes relative positions and names
 	for x: float in probes_x:
 		for y: float in probes_y:
 			for z: float in probes_z:
@@ -177,116 +181,273 @@ func generate_probes() -> void:
 	set_probes_number()
 
 
+# Workaround to raycast without colliders. Consists in a camera with a filter in front that shows 
+# the depth texture. The camera.far is the "ray" lenght and camera rotation is the "ray" orientation
+# https://docs.godotengine.org/en/stable/tutorials/shaders/advanced_postprocessing.html#depth-texture
+func add_GPU_raycaster(probe: Node3D) -> void:
+	var root_node: Node = get_tree().edited_scene_root
+	
+	# SubViewport that will host the camera
+	var sub_viewport: SubViewport = SubViewport.new()
+	sub_viewport.name = "GPUraycast"
+	sub_viewport.size = Vector2(2, 2)
+	sub_viewport.render_target_update_mode = SubViewport.UPDATE_DISABLED
+	sub_viewport.render_target_clear_mode = SubViewport.CLEAR_MODE_NEVER
+	sub_viewport.handle_input_locally = false
+	sub_viewport.debug_draw = Viewport.DEBUG_DRAW_UNSHADED
+	
+	sub_viewport.positional_shadow_atlas_size = 0
+	sub_viewport.positional_shadow_atlas_quad_0 = Viewport.SHADOW_ATLAS_QUADRANT_SUBDIV_DISABLED
+	sub_viewport.positional_shadow_atlas_quad_1 = Viewport.SHADOW_ATLAS_QUADRANT_SUBDIV_DISABLED
+	sub_viewport.positional_shadow_atlas_quad_2 = Viewport.SHADOW_ATLAS_QUADRANT_SUBDIV_DISABLED
+	sub_viewport.positional_shadow_atlas_quad_3 = Viewport.SHADOW_ATLAS_QUADRANT_SUBDIV_DISABLED
+	
+	probe.add_child(sub_viewport)
+	sub_viewport.set_owner(root_node)
+	
+	# Camera for the viewport
+	var camera_3d: Camera3D = Camera3D.new()
+	camera_3d.projection = Camera3D.PROJECTION_ORTHOGONAL
+	camera_3d.size = 0.001
+	camera_3d.near = 0.001
+	camera_3d.far = 1.0
+	sub_viewport.add_child(camera_3d)
+	camera_3d.set_owner(root_node)
+	camera_3d.position = probe.global_position
+	camera_3d.rotation = Vector3.ZERO
+	camera_3d.cull_mask = visual_cull_mask
+	
+	# Depth filter: A quad with a material that shows the Depth texture. This goes in front of the
+	# camera
+	var depth_material: ShaderMaterial = ShaderMaterial.new()
+	depth_material.shader = depth_shader
+	
+	var depth_filter: MeshInstance3D = MeshInstance3D.new()
+	var depth_mesh: QuadMesh = QuadMesh.new()
+	depth_filter.mesh = depth_mesh
+	depth_mesh.material = depth_material
+	depth_mesh.size = Vector2.ONE * 0.001
+	camera_3d.add_child(depth_filter)
+	depth_filter.set_owner(root_node)
+	depth_filter.position = Vector3(0.,0., -0.002)
+	depth_filter.rotation = Vector3.ZERO
+
+
+func generate_probes_raycasters(distance: float) -> void:
+	for probe in get_children():
+		add_GPU_raycaster(probe)
+
+
+func remove_probes_raycasters() -> void:
+	for probe in get_children():
+		for child in probe.get_children():
+			child.queue_free();
+
+
+# The function look_at not always work. Exceptions are handled here
+func rotate_camera(camera: Camera3D, to: Vector3) -> void:
+	var from: Vector3 = camera.position
+	# look_at don't work if the node and target have the same position. You cannot look at yourself
+	if from == to:
+		return
+	
+	# look_at don't work if the direction and rotation axix have same orientation. In this case,
+	# change the rotation axis
+	var direction: Vector3 = abs(to - from)
+	var mag = (direction.normalized() - Vector3.UP).length()
+	if mag > 0.001:
+		camera.look_at(to)
+	else:
+		camera.look_at(to, Vector3.RIGHT)
+
+
 # Shoot rays from the center to all the probes. If any object is detected so the probe is
 # obstructed and will be cut
-func cut_obstructed() -> void:	
-	var space_state: PhysicsDirectSpaceState3D = get_world_3d().direct_space_state
-	var query = PhysicsRayQueryParameters3D.create(position, position, collision_mask, [self])
+func cut_obstructed() -> void:
+	await generate_probes_raycasters(far_distance)
 	
-	for probe: Node3D in get_children():
-		query.to = position + probe.position
-		var result = space_state.intersect_ray(query)
+	var probes_array: Array[LightmapProbe] = []
+	var camera_array: Array[Camera3D] = []
+	var subViewport_array: Array[SubViewport] = []
+	var results_array: Array[float] = []
+	
+	# Populating arrays
+	for probe: LightmapProbe in get_children():
+		probes_array.append(probe)
+		gizmo_lines.append_array([Vector3.ZERO, probe.position])
+		var sub_viewport: SubViewport = probe.get_child(0)
+		subViewport_array.append(sub_viewport)
+		var camera: Camera3D = sub_viewport.get_child(0)
+		camera_array.append(camera)
+	
+	# Rotating cameras and updating sub_viewports
+	for i in range(camera_array.size()):
+		var camera: Camera3D = camera_array[i]
+		var probe_pos: Vector3 = probes_array[i].global_position
+		var sub_viewport: SubViewport = subViewport_array[i]
 		
-		if not result.is_empty():
+		camera.position = position
+		# The lenght of the "Ray"
+		camera.far = (probe_pos - position).length()
+		# The direction of the "Ray"
+		rotate_camera(camera, probe_pos)
+		sub_viewport.render_target_update_mode = SubViewport.UPDATE_ONCE
+	
+	# Getting the values
+	await  RenderingServer.frame_post_draw
+	for i in range(subViewport_array.size()):
+		var sub_viewport = subViewport_array[i]
+		var texture: Image = sub_viewport.get_texture().get_image()
+		var color: Color = texture.get_pixel(0,0)
+		var colorValue: float = color.r
+		var result: float = colorValue
+		results_array.append(result)
+	
+	# Cutting probes
+	for i in range(subViewport_array.size()):
+		var result: float = results_array[i]
+		if result < 1.0:
+			var probe = probes_array[i]
 			probe.queue_free()
 			current_probes -= 1
-		
-		gizmo_lines.append_array([Vector3.ZERO, probe.position])
 	
 	set_probes_number()
+	remove_probes_raycasters()
 
 
 # Detect if the probe is far from any object. It will shoot rays on all 6 axis and 8 quadrants.
 # If there aren't any objects the probe will be cut
-func cut_far():
-	var objects: Array[int] = []
+func cut_far() -> void:
+	await generate_probes_raycasters(far_distance)
 	
-	for probe: Node3D in get_children():
-		objects.clear()
-		objects = test_six_axis(probe, far_distance, true)
-		
-		if objects.size() < 1:
-			probe.queue_free()
-			current_probes -= 1
-	
-	set_probes_number()
-
-
-# Detect if probe is inside an object. It will shoot rays on all 6 axis and if at least 4 are in 
-# the same object so the probe is inside and will be cut
-func cut_inside():
-	var objects: Array[int] = []
-	var object_id: int = -1
-	var count: int = 0
-	
-	# Test the 6 axis of each probe and count the number of times the same object is hit
-	for probe: Node3D in get_children():
-		objects.clear()
-		objects = test_six_axis(probe, object_size, false)
-		
-		object_id = -1
-		count = 0
-		for i: int in range(objects.size()):
-			if object_id == -1: 
-				object_id = objects[i]
-			if objects[i] == object_id:
-				count += 1
-		
-		# Test only 4 sides instead of 6, because is normal to have objects without 2 sides
-		# like pillars
-		if count >= 4:
-			probe.queue_free()
-			current_probes -= 1
-	
-	set_probes_number()
-
-
-## Test all 6 axis and 8 quadrants (optional) of a position and return the id's of objects collided
-func test_six_axis(object: Node3D, distance: float, test_quadrants: bool) -> Array[int]:
-	# 6 Axis
-	var axis: Array[Vector3] = [
+	# 6 axis and 8 quadrants
+	var directions: Array[Vector3] = [
+		# 6 Axis
 			Vector3(0, 0, 1), Vector3(0, 1, 0), Vector3(1, 0, 0), 
-			Vector3(0, 0, -1), Vector3(0, -1, 0), Vector3(-1, 0, 0)
-	]
-	
-	# 8 Quadrants
-	var quadrants: Array[Vector3] = [
+			Vector3(0, 0, -1), Vector3(0, -1, 0), Vector3(-1, 0, 0),
+		# 8 Quadrants
 			Vector3(1, 1, 1).normalized(), Vector3(1, 1, -1).normalized(), 
 			Vector3(1, -1, 1).normalized(),  Vector3(1, -1, -1).normalized(),
 			Vector3(-1, 1, 1).normalized(), Vector3(-1, 1, -1).normalized(), 
 			Vector3(-1, -1, 1).normalized(),  Vector3(-1, -1, -1).normalized()
 	]
 	
-	var axis_and_quadrants: Array[Vector3] = axis
-	if test_quadrants == true:
-		axis_and_quadrants.append_array(quadrants)
+	var probes_array: Array[LightmapProbe] = []
+	var camera_array: Array[Camera3D] = []
+	var subViewport_array: Array[SubViewport] = []
+	var collisions_number: Array[int] = []
 	
-	# Initialize space and query
-	var space_state: PhysicsDirectSpaceState3D = get_world_3d().direct_space_state
-	# Initializing with dummy positions
-	var query := PhysicsRayQueryParameters3D.create(Vector3.ZERO, Vector3.ONE, collision_mask, [self])
-	query.hit_from_inside = true
-	query.hit_back_faces = true
+	# Populating arrays
+	for probe: LightmapProbe in get_children():
+		probes_array.append(probe)
+		var sub_viewport: SubViewport = probe.get_child(0)
+		subViewport_array.append(sub_viewport)
+		var camera: Camera3D = sub_viewport.get_child(0)
+		camera_array.append(camera)
+	collisions_number.resize(camera_array.size())
+	collisions_number.fill(0)
 	
-	# objects id returned by this function
-	var objects: Array[int] = []
-	
-	# Raycasting each axis and quadrant (optional)
-	for pos: Vector3 in axis_and_quadrants:
-		query.from = object.global_position
-		query.to = object.global_position + pos * distance
-		var result = space_state.intersect_ray(query)
+	# Getting data for all cameras on each direction
+	for dir in directions:
+		# Rotating all cameras to the same direction, and updating viewport
+		for i in camera_array.size():
+			var probe: LightmapProbe = probes_array[i]
+			var sub_viewport: SubViewport = subViewport_array[i]
+			var camera: Camera3D = camera_array[i]
+			
+			camera.position = probe.global_position
+			# The lenght of the "Ray"
+			camera.far = far_distance
+			# The direction of the "Ray"
+			rotate_camera(camera, probe.global_position + dir)
+			sub_viewport.render_target_update_mode = SubViewport.UPDATE_ONCE
+			
+			gizmo_lines.append_array([probe.position, probe.position + dir * far_distance])
 		
-		if not result.is_empty():
-			objects.append(result["collider_id"])
-		else:
-			# Making the other way around, because hit_back_faces don't always work
-			query.from = object.global_position + pos * distance
-			query.to = object.global_position
-			result = space_state.intersect_ray(query)
-			if not result.is_empty():
-				objects.append(result["collider_id"])
-		
-		gizmo_lines.append_array([object.position, object.position + pos * distance])
+		# Getting all values for the current direction
+		await  RenderingServer.frame_post_draw
+		for i in range(subViewport_array.size()):
+			var sub_viewport = subViewport_array[i]
+			var texture: Image = sub_viewport.get_texture().get_image()
+			var color: Color = texture.get_pixel(0,0)
+			var colorValue: float = color.r
+			var result: float = colorValue
+			if result < 1.0:
+				collisions_number[i] += 1
 	
-	return objects
+	# Cut probes if there are no collisions
+	for i in probes_array.size():
+		if collisions_number[i] < 1:
+			var probe = probes_array[i]
+			probe.queue_free()
+			current_probes -= 1
+	
+	set_probes_number()
+	remove_probes_raycasters()
+
+
+# Detect if probe is inside an object. It will shoot rays from all 6 axis to the probe. If at least
+# 4 are obstructed, the probe will be cut
+func cut_inside() -> void:
+	await generate_probes_raycasters(far_distance)
+	
+	# 6 Axis
+	var axis: Array[Vector3] = [
+			Vector3(0, 0, 1), Vector3(0, 1, 0), Vector3(1, 0, 0), 
+			Vector3(0, 0, -1), Vector3(0, -1, 0), Vector3(-1, 0, 0),
+	]
+	
+	var probes_array: Array[LightmapProbe] = []
+	var camera_array: Array[Camera3D] = []
+	var subViewport_array: Array[SubViewport] = []
+	var collisions_number: Array[int] = []
+	
+	# Populating arrays
+	for probe: LightmapProbe in get_children():
+		probes_array.append(probe)
+		var sub_viewport: SubViewport = probe.get_child(0)
+		subViewport_array.append(sub_viewport)
+		var camera: Camera3D = sub_viewport.get_child(0)
+		camera_array.append(camera)
+	collisions_number.resize(camera_array.size())
+	collisions_number.fill(0)
+	
+	# Getting data for all cameras on each axis
+	for dir in axis:
+		# For each direction, position all cameras to look from outside 
+		# to each probe in object_size distance
+		for i in camera_array.size():
+			var probe: LightmapProbe = probes_array[i]
+			var sub_viewport: SubViewport = subViewport_array[i]
+			var camera: Camera3D = camera_array[i]
+			
+			camera.position = probe.global_position + dir * object_size
+			# The lenght of the "Ray"
+			camera.far = object_size
+			# The direction of the "Ray"
+			rotate_camera(camera, probe.global_position)
+			sub_viewport.render_target_update_mode = SubViewport.UPDATE_ONCE
+			
+			gizmo_lines.append_array([probe.position, probe.position + dir * object_size])
+		
+		# Getting all values for the current direction
+		await  RenderingServer.frame_post_draw
+		for i in range(subViewport_array.size()):
+			var sub_viewport = subViewport_array[i]
+			var texture: Image = sub_viewport.get_texture().get_image()
+			var color: Color = texture.get_pixel(0,0)
+			var colorValue: float = color.r
+			var result: float = colorValue
+			if result < 1.0:
+				collisions_number[i] += 1
+	
+	# Cut probes if there are more than 4 collisions
+	for i in probes_array.size():
+		if collisions_number[i] > 3:
+			var probe = probes_array[i]
+			probe.queue_free()
+			current_probes -= 1
+	
+	set_probes_number()
+	remove_probes_raycasters()
+
